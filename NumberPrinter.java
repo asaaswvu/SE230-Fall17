c9ec7b10344df3757f13e41bab6412a135e9ff3d
Dallas Scott Klages <dklages1@ubuntu.statler.wvu.edu>
2017-09-05 18:37:52 -0400

    I added functionality that would sort the linked list in ascending order and then print the linked list's contents out to the user

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.*;
 
 class NumberPrinter{
 
@@ -17,7 +18,12 @@ class NumberPrinter{
     }
 
     private void doStuff(){
-        
+        Collections.sort(ll); // sorts the linked list in ascending order
+
+	for(int i = 0; i<ll.size(); i++){
+
+		System.out.println(ll.get(i));
+	}
     }
 
 
