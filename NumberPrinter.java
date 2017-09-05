df8d2d2a38541b5b5fb11ccb850d276747c4e78e
Geoffrey Jaymes Houy <gjhouy@ubuntu.statler.wvu.edu>
2017-09-05 18:33:04 -0400

    Added functionality to the doStuff() method

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -8,20 +9,20 @@ class NumberPrinter{
     NumberPrinter(){
         Random rand = new Random();
         ll = new LinkedList<Integer>();
-        
-        for(int i=0; i<10; i++){
-            ll.add(rand.nextInt(100) + 1);
+	
+        for (int i=0; i<10; i++){
+		ll.add(rand.nextInt(100) + 1);
         }
 
         doStuff();
     }
 
     private void doStuff(){
-        
+        Collections.sort(ll);
+	for (Integer l : ll) {
+		System.out.println(l);
+	}
     }
-
-
-
 
     public static void main(String [] args){
         new NumberPrinter();	
